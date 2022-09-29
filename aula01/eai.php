200d2aee55857424e2427b4e493d9df984fd789f
Pedro Paulo Garcia Age <pedropaulogarciabr@gmail.com>
2022-09-28 21:30:55 -0300

    começo de PHP

---
+++ aula01/eai.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="estilo/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $linguagem = "mijão";
+
+    echo "<h1>Eai, o Pedro é muito gato e cheiroso e o guigui é um " .$linguagem ."</h1>";
+
+    printf("<h3>Eai, o Pedro realmente é incrivel mesmo e é impressionante como o guigui é " .$linguagem ."</h3>");
+  
+    ?>
+    <?php
+    
+    $numero1="11";
+    $numero2="11";
+
+    echo " a soma de ".$numero1. " + " .$numero2. " = " . ($numero1 + $numero2);
+    
+    ?>
+</body>
+</html>
